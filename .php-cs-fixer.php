e256a679bdc976b9e32a8e0ce98c8dad0e8b9a5c
Mohammad Emran <memran.dhk@gmail.com>
2026-03-31 16:19:58 +0600

    Add static analysis and CI tooling

---
+++ .php-cs-fixer.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->append([__FILE__]);
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'declare_strict_types' => true,
+        'no_unused_imports' => true,
+        'ordered_imports' => true,
+        'single_quote' => true,
+    ])
+    ->setFinder($finder);
